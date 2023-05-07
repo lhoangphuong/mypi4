356716a9dde0d0068818e1f8054adcb96f5dd976
lhoangphuong <49200808+lhoangphuong@users.noreply.github.com>
2023-05-07 16:17:20 +0700

    update

--- script/setup.sh
+++ script/setup.sh
@@ -2,6 +2,9 @@
 
 sudo apt update
 sudo apt full-upgrade
+sudo apt install unattended-upgrades -y
+echo 'APT::Periodic::Update-Package-Lists "1";' | sudo tee /etc/apt/apt.conf.d/20auto-upgrades > /dev/null
+echo 'APT::Periodic::Unattended-Upgrade "1";' | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades > /dev/null
 
 sudo curl -fsSL -o "/boot/config.txt" "https://raw.githubusercontent.com/lhoangphuong/mypi4/main/boot/config.txt"
 
