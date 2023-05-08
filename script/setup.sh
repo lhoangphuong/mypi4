9d1c2ac7c36cfcfaf62fed02e4ed74fae9d75caf
lhoangphuong <49200808+lhoangphuong@users.noreply.github.com>
2023-05-08 18:31:30 +0700

    update

--- script/setup.sh
+++ script/setup.sh
@@ -2,6 +2,7 @@
 
 sudo apt update
 sudo apt full-upgrade
+sudo apt install cmake -y
 sudo apt install unattended-upgrades -y
 echo 'APT::Periodic::Update-Package-Lists "1";' | sudo tee /etc/apt/apt.conf.d/20auto-upgrades > /dev/null
 echo 'APT::Periodic::Unattended-Upgrade "1";' | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades > /dev/null
@@ -14,4 +15,8 @@ rm get-docker.sh
 sudo usermod -aG docker $USER
 newgrp docker
 
-sudo apt install cockpit
+sudo apt install cockpit -y
+
+sudo apt install zsh -y
+#chsh -s $(which zsh)
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
